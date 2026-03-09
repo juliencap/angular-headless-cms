6a3c1ed3b704dc73a8c2fb851a591e8849957db3
Julien Cap <4956238+juliencap@users.noreply.github.com>
2026-03-09 16:20:19 +0100

    fix: add environment files for deployment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://www.girondins33.com/wp-json/wp/v2'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://www.girondins33.com/wp-json/wp/v2',
+};
