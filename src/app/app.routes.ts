20c27bccd10f5d03d7ffc8b0d1e4bc0a7a98f6bd
Julien Cap <4956238+juliencap@users.noreply.github.com>
2026-03-17 10:35:14 +0100

    feat: add lazy loading for standalone routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,19 @@
 import { Routes } from '@angular/router';
-import { ActualiteList } from './components/actualite-list/actualite-list';
-import { ActualiteDetail } from './components/actualite-detail/actualite-detail';
-import { CalendrierComponent } from './components/calendrier/calendrier';
 
 export const routes: Routes = [
-  { path: '', component: ActualiteList },
-  { path: 'Actualites/:idslug', component: ActualiteDetail },
-  { path: 'calendrier', component: CalendrierComponent },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./components/actualite-list/actualite-list').then((m) => m.ActualiteList),
+  },
+  {
+    path: 'Actualites/:idslug',
+    loadComponent: () =>
+      import('./components/actualite-detail/actualite-detail').then((m) => m.ActualiteDetail),
+  },
+  {
+    path: 'calendrier',
+    loadComponent: () =>
+      import('./components/calendrier/calendrier').then((m) => m.CalendrierComponent),
+  },
 ];
